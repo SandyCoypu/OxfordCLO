cccc161b7523a6911d45a40074fa045bf527dbb4
mayakovksy13 <michaelseankrolak@gmail.com>
2019-07-10 11:29:38 +0100

    exercise 7

---
+++ wc-job.py
@@ -0,0 +1,21 @@
+# ~/spark/bin/spark-submit --master local[*] wc.py "hdfs://localhost:54310/user/oxclo/books/*"
+
+from pyspark import SparkContext, SparkConf
+import sys
+import unicodedata
+
+conf = SparkConf().setAppName("wordCount")
+sc = SparkContext(conf=conf)
+
+
+books = sc.textFile(sys.argv[1])
+split = books.flatMap(lambda line: line.split())
+ascii = split.map(lambda u: str(unicodedata.normalize('NFKD', u).encode('ascii','ignore')))
+stripped = ascii.map(lambda input: ''.join(filter(str.isalpha, input)))
+numbered = stripped.map(lambda word: (word, 1))
+wordcount = numbered.reduceByKey(lambda a,b: a+b)
+
+for k,v in wordcount.collect():
+  print k,v
+
+sc.stop()
